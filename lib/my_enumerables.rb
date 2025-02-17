a478f22678101b6cb22b02a763437ed7a747cba7
Gavin OMelia <gavin@omelia.org>
2025-02-17 13:41:21 -0500

    Complete each

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -29,5 +29,10 @@ end
 # your enumerable module will have access
 # to this method
 class Array
-  # Define my_each here
+  def my_each
+    for element in self
+      yield(element)
+    end
+    self
+  end
 end
